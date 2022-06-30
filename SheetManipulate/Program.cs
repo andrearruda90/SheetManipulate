bed6f2a1317d3ee7efbb22bce55dd853ea31213a
André Arruda <andre.todimo@gmail.com>
2022-06-30 14:12:40 -0400

    improvments

--- SheetManipulate/Program.cs
+++ SheetManipulate/Program.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
 using _Excel = Microsoft.Office.Interop.Excel;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 
 namespace myProject
 {
@@ -23,38 +25,75 @@ namespace myProject
                 excelProcessID.Add(p.Id);
             }
 
+            
 
             OpenFlie();
-            
+            KillProcess();
+
 
             void OpenFlie()
             {
-                Excel excel = new Excel(@$"C:\Users\andre\Desktop\testando.xlsx", 1);
+                Excel excel = new Excel(@$"C:\Users\andre\Desktop\Lista de emails.xlsx", 1);
                 
                 Console.WriteLine(excel.ReadCell(0, 0));
-                excel.WriteCell("C#", 1, 0);
+                excel.WriteCell("C#", 1, 0); // text to write, row, column
                 Console.WriteLine(excel.ReadCell(1, 0));
+
+                string term = @$"gmail";
+                int count = 1;
+                do
+                {
+                    if (excel.ReadCell(count,1).ToString().Contains(term) == true)
+                    {
+                        excel.WriteCell(excel.ReadCell(count, 1),count,1);
+                    }
+                    count++;
+                } while (count <= excel.LastRow());
+
 
                 excel.SaveFile();
             }
 
-            int count = 0;
-            foreach (Process xlProcess in Process.GetProcessesByName("EXCEL"))
+            void KillProcess()
             {
-                foreach (int xlid in excelProcessID)
-                {
-                    if (xlProcess.Id == xlid)
-                    {
-                        count++;
-                    }
-                }
 
-                if (count == 0)
+                int count = 0;
+                foreach (Process xlProcess in Process.GetProcessesByName("EXCEL"))
                 {
-                    xlProcess.Kill();
+                    foreach (int xlid in excelProcessID)
+                    {
+                        if (xlProcess.Id == xlid)
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count == 0)
+                    {
+                        xlProcess.Kill();
+                    }
+                    count = 0;
                 }
-                count = 0;
             }
+
+
+            //int count = 0;
+            //foreach (Process xlProcess in Process.GetProcessesByName("EXCEL"))
+            //{
+            //    foreach (int xlid in excelProcessID)
+            //    {
+            //        if (xlProcess.Id == xlid)
+            //        {
+            //            count++;
+            //        }
+            //    }
+
+            //    if (count == 0)
+            //    {
+            //        xlProcess.Kill();
+            //    }
+            //    count = 0;
+            //}
 
         }
         
@@ -88,6 +127,8 @@ namespace myProject
             j++;
 
             ws.Cells[i, j].Value2 = k;
+            ws.Cells[i, j].Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red); 
+
             return k;
 
             
@@ -98,6 +139,28 @@ namespace myProject
         {
             wb.SaveAs2(@$"C:\Users\andre\Desktop\testando2.xlsx");
         }
+
+        public int LastRow()
+        {
+            _Excel.Range last = ws.Cells.SpecialCells(_Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            _Excel.Range range = ws.get_Range("A1", last);
+
+            int lastUsedRow = last.Row;
+            
+            return lastUsedRow;
+        }
+
+        public int LastColumn()
+        {
+            _Excel.Range last = ws.Cells.SpecialCells(_Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            _Excel.Range range = ws.get_Range("A1", last);
+
+            int lastUsedcolumn = last.Column;
+
+            return lastUsedcolumn;
+        }
+
+        
 
     }
 }
